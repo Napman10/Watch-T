4fab23f57efcd12d0d7fcbea8070f3082f180953
ilia.kashapov <ilia.kashapov@tages.ru>
2021-02-02 13:17:32 +0500

    added initial field to models

--- WatchT/WatchT/apps/task/models.py
+++ WatchT/WatchT/apps/task/models.py
@@ -1,5 +1,24 @@
 from ..abstract.models import BaseModel
+from ..user.models import User, Team
 from django.db.models import Manager
+from django.db import models
+
+
+class Project(BaseModel):
+    object = Manager()
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+
+    short_name = models.CharField(max_length=16, verbose_name='Короткое название', unique=True)
+    header = models.CharField(max_length=65, verbose_name='Заголовок', default='Не подписано')
+    description = models.CharField(max_length=255, verbose_name='Описание', null=True, blank=True)
+    team = models.ForeignKey(Team, verbose_name='Команда, работающая над проектом',
+                             on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.short_name
 
 
 class Task(BaseModel):
@@ -8,3 +27,15 @@ class Task(BaseModel):
     class Meta:
         verbose_name = 'Задача'
         verbose_name_plural = 'Задачи'
+
+    short_name = models.CharField(max_length=16, verbose_name='Короткое название', unique=True)
+    header = models.CharField(max_length=65, verbose_name='Заголовок', null=True, blank=True)
+    description = models.CharField(max_length=255, verbose_name='Описание', null=True, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(User, verbose_name='Составил', on_delete=models.SET_NULL,
+                               null=True, related_name='author_task')
+    executor = models.ForeignKey(User, verbose_name='Выполняет', on_delete=models.SET_NULL,
+                                 null=True, related_name='executor_task')
+
+    def __str__(self):
+        return self.short_name
--- WatchT/WatchT/apps/user/models.py
+++ WatchT/WatchT/apps/user/models.py
@@ -1,5 +1,6 @@
 from ..abstract.models import BaseModel
 from django.db.models import Manager
+from django.db import models
 
 
 class User(BaseModel):
@@ -8,3 +9,36 @@ class User(BaseModel):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+    first_name = models.CharField(max_length=255, verbose_name='Имя', null=True, blank=True)
+    last_name = models.CharField(max_length=255, verbose_name='Фамилия', null=True, blank=True)
+    username = models.CharField(max_length=30, verbose_name='Никнейм', unique=True)
+    password = models.CharField(max_length=255, verbose_name='Пароль')
+
+    def __str__(self):
+        return self.username
+
+
+class Team(BaseModel):
+    objects = Manager()
+
+    class Meta:
+        verbose_name = 'Команда сотрудников'
+        verbose_name_plural = 'Команды сотрудников'
+
+    name = models.CharField(max_length=65, verbose_name='Название', unique=True)
+    description = models.CharField(max_length=255, verbose_name='Описание команды', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User2Team(BaseModel):
+    objects = Manager()
+
+    class Meta:
+        verbose_name = 'Связка Пользователь -> Команда'
+        verbose_name_plural = 'Связки Пользователь -> Команда'
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, verbose_name='Команда')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
