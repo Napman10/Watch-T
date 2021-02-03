30eb14f6bc60975ef905012b30978884dca67e1d
ilia.kashapov <ilia.kashapov@tages.ru>
2021-02-03 12:16:35 +0500

    inited frontend

---
+++ frontend/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import App from './App.vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/ru-RU';
+import store from '@/store';
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
