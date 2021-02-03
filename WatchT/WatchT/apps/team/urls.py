bd44967341ce6d32788f64ccce64d60eda7a0391
ilia.kashapov <ilia.kashapov@tages.ru>
2021-02-03 18:40:03 +0500

    start team api

--- WatchT/WatchT/apps/team/urls.py
+++ WatchT/WatchT/apps/team/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path, include
+from django.urls import path
+from .views import TeamListView, TeamOpenView, TeamCreateView, TeamDestroyView
 
 urlpatterns = [
-
+    path('list', TeamListView.as_view()),
+    path('<uuid:id>', TeamOpenView.as_view()),
+    path('new', TeamCreateView.as_view()),
+    path('delete/<uuid:id>', TeamDestroyView.as_view()),
 ]
--- WatchT/WatchT/apps/team/views.py
+++ WatchT/WatchT/apps/team/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView, CreateAPIView, DestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Team
+from .serializers import TeamSerializer
 
-# Create your views here.
+
+class TeamListView(ListAPIView):
+    serializer_class = TeamSerializer
+
+    def get_queryset(self):
+        return Team.objects.all()
+
+
+class TeamOpenView(RetrieveUpdateAPIView):
+    serializer_class = TeamSerializer
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return Team.objects.all()
+
+
+class TeamCreateView(CreateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+    action_map = {
+        'post': 'create'
+    }
+
+
+class TeamDestroyView(DestroyAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+    lookup_field = 'id'
+    action_map = {
+        'delete': 'delete'
+    }
+
