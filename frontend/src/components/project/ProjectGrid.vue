c3cb564baf40f17da344a0e150cac3ef974f474c
ilia.kashapov <ilia.kashapov@tages.ru>
2021-02-18 19:54:46 +0500

    project grid short_name 100

--- frontend/src/components/project/ProjectGrid.vue
+++ frontend/src/components/project/ProjectGrid.vue
@@ -4,7 +4,7 @@
             :data="projects"
             style="width: 100%"
         >
-            <el-table-column prop="short_name" width="50" />
+            <el-table-column prop="short_name" width="100" />
             <el-table-column prop="header"  width="400" />
         </el-table>
     </div>
