22ff0c2cd376621e480abca0b24bacf33d6dc132
Napman10 <ilya.kashapov99@gmail.com>
2021-03-11 19:08:03 +0500

    assign project back

--- backend/WatchT/apps/project/urls.py
+++ backend/WatchT/apps/project/urls.py
@@ -1,11 +1,12 @@
-from django.urls import include, path
+from django.urls import path
 
 from .views import (ProjectCreateView, ProjectDestroyView, ProjectListView,
-                    ProjectOpenView)
+                    ProjectOpenView, Project2UserView)
 
 urlpatterns = [
     path('list', ProjectListView.as_view()),
     path('<uuid:id>', ProjectOpenView.as_view()),
     path('new', ProjectCreateView.as_view()),
     path('delete/<uuid:id>', ProjectDestroyView.as_view()),
+    path('assign/', Project2UserView.as_view()),
 ]
--- backend/WatchT/apps/project/views.py
+++ backend/WatchT/apps/project/views.py
@@ -1,8 +1,13 @@
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveUpdateAPIView)
-from .models import Project
+from .models import Project, Project2User
 from .serializers import ProjectSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import APIException
+from ..user.models import EmployeeUser
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
 
 class ProjectListView(ListAPIView):
@@ -39,3 +44,28 @@ class ProjectDestroyView(DestroyAPIView):
     action_map = {
         'delete': 'delete'
     }
+
+
+class Project2UserView(APIView):
+
+    def initialize(self, request):
+        data = request.data
+        user_id = data.get('user_id')
+        if not user_id:
+            raise APIException
+        project_id = data.get('project_id')
+        if not project_id:
+            raise APIException
+        user = EmployeeUser.objects.get(id=user_id)
+        project = Project.objects.get(id=project_id)
+        return user, project
+
+    def post(self, request):
+        user, project = self.initialize(request)
+        Project2User.objects.create(user=user, project=project)
+        return Response(status=status.HTTP_201_CREATED)
+
+    def delete(self, request):
+        user, project = self.initialize(request)
+        Project2User.objects.filter(user=user, project=project).first().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
