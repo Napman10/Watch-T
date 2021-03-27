773b977cb3d56a9663c0694fa7ce16c77a9b9c68
Napman10 <ilya.kashapov99@gmail.com>
2021-03-27 15:24:36 +0500

    navbar

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -1,20 +1,34 @@
 <template>
-    <header class="header">
-        <router-link class="logo" to='/' exact>WatchT</router-link>
-        <router-link to='/issues' exact>Задачи</router-link>
-        <router-link :to="/user/ + myId()" exact>Профиль</router-link>
-        <router-link to='/projects' exact>Проекты</router-link>
-        <router-link v-if="meAdmin()" to='/users' exact>Пользователи</router-link>
-        <menu class="userInfo">
-            <li class="cursor-pointer" @click="logout">Выйти</li>
-        </menu>
-    </header>
+  <el-menu
+      :default-active="activeIndex2"
+      id="nav"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#6A5ACD"
+      text-color="#fff"
+      :router="true"
+      active-text-color="#FFDEAD">
+    <el-menu-item style="margin-left: 10px" index="/issues" :route="{ name: 'issues'}">Задачи</el-menu-item>
+    <el-menu-item index="/projects" :route="{ name: 'projects'}">Проекты</el-menu-item>
+    <el-menu-item v-if="meAdmin()" index="/users" :route="{ name: 'users'}">Пользователи</el-menu-item>
+    <el-submenu style="position: absolute; right: .5em;" index="2">
+      <template slot="title">Профиль</template>
+      <el-menu-item index="/user" :route="{ path: 'user/'+myId()}">Мой профиль</el-menu-item>
+      <el-menu-item index="/logout" @click="logout">Выйти</el-menu-item>
+    </el-submenu>
+  </el-menu>
 </template>
 
 <script>
 import {meAdmin, meCreator} from "@/utils/indentMe";
 
 export default {
+  data() {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    };
+  },
   name: "PageHeader",
   computed: {
     },
@@ -31,19 +45,7 @@ export default {
 </script>
 
 <style scoped>
-.header {
-    background: #412227;
-    padding: 15px 15px;
-    margin-bottom: 10px;
-}
-.logo {
-    color: white;
-    font-weight: bold;
-    font-size: x-large;
-    text-decoration: none;
-}
-.cursor-pointer {
-    color: white;
-    cursor: pointer;
-}
+  #nav{
+    margin-bottom: 10px
+  }
 </style>
